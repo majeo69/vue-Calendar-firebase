74609d5a43f4648a83319b6fd3b3cf7e152b9997
majeo69 <alexgutscher26@gmail.com>
2020-08-20 19:16:57 -0500

    test

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,10 +3,17 @@
     <v-col>
       <v-sheet height="64">
         <v-toolbar flat color="white">
-          <v-btn color="primary" dark @click.stop="dialog = true">
+          <v-btn color="primary" dark @click.stop="dialog = true"
+           class="text-center grey lighten-2 rounded-pill d-inline-flex align-center justify-center ma-3"
+           height="64"
+           width="100"
+        >
             New Event
           </v-btn>
-          <v-btn outlined class="mr-4" @click="setToday">
+          <v-btn outlined class="mr-4" @click="setToday"
+           height="64"
+           width="100"
+        >
             Today
           </v-btn>
           <v-btn fab text small @click="prev">
@@ -90,6 +97,11 @@
   @click:more="viewDay"
   @click:date="setDialogDate"
   @change="updateRange"
+  @mousedown:event="startDrag"
+  @mousedown:time="startTime"
+  @mousemove:time="mouseMove"
+  @mouseup:time="endDrag"
+  @mouseleave.native="cancelDrag"
   ></v-calendar>
   <v-menu
   v-model="selectedOpen"
@@ -106,7 +118,6 @@
       <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
       <div class="flex-grow-1"></div>
     </v-toolbar>
-
     <v-card-text>
       <form v-if="currentlyEditing !== selectedEvent.id">
         {{ selectedEvent.details }}
@@ -139,6 +150,43 @@
 </v-col>
 </v-row>
 </template>
+
+<style scoped lang="scss">
+.v-event-draggable {
+  padding-left: 6px;
+}
+
+.v-event-timed {
+  user-select: none;
+  -webkit-user-select: none;
+}
+
+.v-event-drag-bottom {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 4px;
+  height: 4px;
+  cursor: ns-resize;
+
+  &::after {
+    display: none;
+    position: absolute;
+    left: 50%;
+    height: 4px;
+    border-top: 1px solid white;
+    border-bottom: 1px solid white;
+    width: 16px;
+    margin-left: -8px;
+    opacity: 0.8;
+    content: '';
+  }
+
+  &:hover::after {
+    display: block;
+  }
+}
+</style>
 
 <script>
 import { db } from '@/main'
--- src/main.js
+++ src/main.js
@@ -9,13 +9,14 @@ Vue.use(VueTextareaAutosize)
 Vue.config.productionTip = false
 
 firebase.initializeApp({
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+    apiKey: "AIzaSyD9q8CVnC5fM2EcBeD7O_ubQx44ZQVgzLc",
+    authDomain: "vue-calender-a0883.firebaseapp.com",
+    databaseURL: "https://vue-calender-a0883.firebaseio.com",
+    projectId: "vue-calender-a0883",
+    storageBucket: "vue-calender-a0883.appspot.com",
+    messagingSenderId: "89478819565",
+    appId: "1:89478819565:web:6979112767036f7eb5a6e4",
+    measurementId: "G-0V7JLPLN6W"
 });
 
 export const db = firebase.firestore();
